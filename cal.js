0aa20e102e73b5f8770687b9f307f2be8784e2b9
my-negar <246271318+my-negar@users.noreply.github.com>
2025-12-11 02:09:25 -0800

    add js

---
+++ cal.js
@@ -0,0 +1,33 @@
+let inputBox = document.getElementById("inputBox")
+let buttons = document.querySelectorAll("button")
+
+let string = '';
+
+buttons.forEach(element => {
+    element.addEventListener("click" , (b) => {
+        
+        //=
+        if(b.target.innerText == '='){
+            string = String(eval(string));
+            inputBox.value = string;
+        }
+
+        // C
+        else if(b.target.innerText == "C")
+          { string = " ";
+        inputBox.value = string;}
+
+        // +/-
+        else if(b.target.id == "plusMinus"){
+            string = String(-eval(string));
+            inputBox.value = string;
+        }
+
+        //anoder 
+
+        else {
+            string += b.target.innerText;
+            inputBox.value = string;
+         }
+     });
+ });
