dda5bb4f4d28113681a24c1063c9efe1dc4dd0ac
my-negar <246271318+my-negar@users.noreply.github.com>
2025-12-14 01:08:29 -0800

    add js

--- todolist/list.js
+++ todolist/list.js
@@ -1,22 +1,37 @@
-
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
 const list = document.getElementById("list");
 
-btn.addEventListener("click" , function()
-{ const value = input.value;
-    if( value === " " ) return;
-const li = document.createElement("li");
-li.innerText=value;
-});
-li.addEventListener("click" , function(){
-    li.remove();
-});
+btn.addEventListener("click",
+    function () {
+        const value = input.value;
+        if (value === " ") return;
 
-list.appendChild(li);
-input.value = " ";
+        const li = document.createElement("li");
+        li.innerText = value;
 
-input.addEventListener("keypress" , 
-    function(e){
-    if (e.key === "Enter") btn.click();
-});
+        li.addEventListener("click", function () {
+            li.classList.toggle("done");
+        });
+
+        li.addEventListener("dblclick", function () {
+            li.remove();
+        });
+
+        list.appendChild(li);
+        input.value = " ";
+    });
+
+input.addEventListener("keydown",
+    function (event) {
+        if (event.key === "Enter") {
+            btn.click();
+        }
+    });
+
+
+
+
+
+
+
